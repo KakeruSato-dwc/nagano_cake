386e30cd483eabdee85791d81948e1d0f5cea5b4
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-10-17 13:54:14 +0000

    Fix access_restriction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!, if: :admin_url
-  before_action :authenticate_customer!, unless: :item_url
 
   def admin_url
     request.fullpath.include?("/admin")
-  end
-  
-  def item_url
-    request.fullpath.include?("/items")
   end
 end
