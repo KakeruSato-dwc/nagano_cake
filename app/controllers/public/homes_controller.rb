495ecf35bf1eea01c248f12fe8dd3da5fdc0ad9f
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-10-18 18:46:12 +0000

    Finish Item

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @customers = Customer.all.order(created_at: :desc)
-    @customers_first = @customers.first(2)
+    @items = Item.all.order(created_at: :desc)
+    @items_first = @items.first(4)
   end
 
   def about
